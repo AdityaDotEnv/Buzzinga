d2ef180efa951aae1ef7ae33aab9b68b6b33bdf5
Aditya K <adityak.10102005@gmail.com>
2026-09-18 20:59:13 +0530

    feat(db): migrated database from MongoDB Atlas to local mongosh config

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
+const DEFAULT_LOCAL_MONGO_URI = 'mongodb://127.0.0.1:27017/buzzinga';
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || '');
+    const conn = await mongoose.connect(process.env.LOCAL_MONGO_URI || DEFAULT_LOCAL_MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${(error as Error).message}`);
